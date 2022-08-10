ddca10eafac6f0b8b9e9241c177fa4c0663f04c1
dominica-of <dominicaamanfo@gmail.com>
2022-08-10 20:40:34 +0000

    static

--- stedom/settings.py
+++ stedom/settings.py
@@ -137,7 +137,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_DIRS = [
+    'staticfiles/'
+]
 
 # Enable WhiteNoise's GZip compression of static assets.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
