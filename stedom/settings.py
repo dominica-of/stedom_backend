2234a9afc52c23ed0d57de38867031ff5ef0c7c5
dominica-of <dominicaamanfo@gmail.com>
2022-08-10 20:16:57 +0000

    static files issues

--- stedom/settings.py
+++ stedom/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = STATICFILES_DIR
 
 # Enable WhiteNoise's GZip compression of static assets.
