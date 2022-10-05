6f452d9512cba0d2791160da7ba75ab42ea39689
dominica-of <dominicaamanfo@gmail.com>
2022-10-05 00:23:29 +0000

    static

--- stedom/settings.py
+++ stedom/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'easyaudit.middleware.easyaudit.EasyAuditMiddleware',
 ]
 
